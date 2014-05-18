43c3cd6237c4b75ee502734517bed12cab31d058
Peter Monks <pmonks@gmail.com>
2014-05-18 14:26:54 -0700

    Added description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject alfresco-amp-template/lein-template "0.1.0"
-  :description "FIXME: write description"
+  :description "Leiningen template for AMP (Alfresco Module Package) projects."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
