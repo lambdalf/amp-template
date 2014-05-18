76edfc3fa7b7616f26af436090a5083bb97f74a1
Peter Monks <pmonks@gmail.com>
2014-05-18 15:19:24 -0700

    Fixed bug in replacement parameters in project.clj.  Added more to the template.

--- src/leiningen/new/amp.clj
+++ src/leiningen/new/amp.clj
@@ -2,21 +2,27 @@
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :as main]))
 
-(def render (renderer "amp"))
-
 (defn amp
   "The lein-new template for Alfresco Module Package (AMP) projects."
   [name]
   (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info (str "Creating new AMP project '" name "'..."))
+              :sanitized (name-to-path name)}
+        render #((renderer "amp") % data)]
+    (main/info (str "Generating a project called " name " based on the 'amp' template."))
     (->files data
              [".gitignore"  (render "gitignore")]
              ["project.clj" (render "project.clj")]
              ["README.md"   (render "README.md")]
+             ["LICENSE"     (render "LICENSE")]
              "amp/web"
-             "amp/resources"
+             "amp/web/css/{{sanitized}}"
+             "amp/web/images/{{sanitized}}"
+             "amp/web/scripts/{{sanitized}}"
+             "amp/config"
+             "amp/config/alfresco/extension/templates/webscripts"
              "amp/module"
+             ["amp/module/module-context.xml" (render "module-context.xml")]
+             "amp/module/contexts"
              "amp/licenses"
              "src/{{sanitized}}"
              "test/{{sanitized}}")))
