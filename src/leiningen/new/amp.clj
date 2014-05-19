75b6446d1b3c4cb9f67f1b85be026bfc6b4a68c9
Peter Monks <pmonks@gmail.com>
2014-05-18 21:14:06 -0700

    Added support for namespaces in project name

--- src/leiningen/new/amp.clj
+++ src/leiningen/new/amp.clj
@@ -1,21 +1,28 @@
 (ns leiningen.new.amp
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.new.templates :refer [renderer year project-name
+                                             ->files sanitize-ns name-to-path
+                                             multi-segment]]
             [leiningen.core.main :as main]))
 
 (defn amp
   "The lein-new template for Alfresco Module Package (AMP) projects."
   [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}
-        render #((renderer "amp") % data)]
-    (main/info (str "Generating a project called " name " based on the 'amp' template."))
+  (let [render  (renderer "amp")
+        main-ns (multi-segment (sanitize-ns name))
+        data    { :raw-name    name
+                  :name        (project-name name)
+                  :namespace   main-ns
+                  :nested-dirs (name-to-path main-ns)
+                  :year        (year) }]
+    (main/info "Generating a project called" name "based on the 'amp' template.")
+    (main/info "To see other templates (app, plugin, etc), try `lein help new`.")
     (->files data
-             [".gitignore"  (render "gitignore")]
-             ["project.clj" (render "project.clj")]
-             ["README.md"   (render "README.md")]
-             ["LICENSE"     (render "LICENSE")]
+             [".gitignore"  (render "gitignore" data)]
+             ["project.clj" (render "project.clj" data)]
+             ["README.md"   (render "README.md" data)]
+             ["LICENSE"     (render "LICENSE" data)]
              "amp"
-             ["amp/module.properties" (render "module.properties")]
+             ["amp/module.properties" (render "module.properties" data)]
              "amp/web"
              "amp/web/css/{{sanitized}}"
              "amp/web/images/{{sanitized}}"
@@ -23,8 +30,8 @@
              "amp/config"
              "amp/config/alfresco/extension/templates/webscripts"
              "amp/module"
-             ["amp/module/module-context.xml" (render "module-context.xml")]
-             "amp/module/contexts"
+             ["amp/module/module-context.xml" (render "module-context.xml" data)]
+             "amp/module/context"
              "amp/licenses"
              "src/{{sanitized}}"
              "test/{{sanitized}}")))
