f9a95a7e11d91ea8d6e54f05702dd702c87984fb
Peter Monks <pmonks@gmail.com>
2014-07-09 14:36:21 -0700

    Rolled lein-amp dependency to v2.0

--- src/leiningen/new/amp/project.clj
+++ src/leiningen/new/amp/project.clj
@@ -5,7 +5,7 @@
   :license { :name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html" }
   :min-lein-version "2.0.0"
-  :profiles { :dev { :plugins [[lein-amp "0.2.0-SNAPSHOT"]] }
+  :profiles { :dev { :plugins [[lein-amp "0.2.0"]] }
               :provided { :dependencies [
                                           [org.clojure/clojure "1.6.0" :scope "runtime"]
 ;                                          [org.clojars.lambdalf/lambdalf "2.0.0-SNAPSHOT" :scope "runtime"]   ; Not yet available on clojars...
