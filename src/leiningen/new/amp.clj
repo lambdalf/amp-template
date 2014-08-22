f8a9bb08f39bd9fafbb46ddc04a2549ff3db014d
Peter Monks <pmonks@gmail.com>
2014-08-22 15:52:29 -0700

    Modifications in support of https://github.com/lambdalf/lein-amp/issues/10

--- src/leiningen/new/amp.clj
+++ src/leiningen/new/amp.clj
@@ -1,5 +1,5 @@
 (ns leiningen.new.amp
-  (:require [leiningen.new.templates :refer [renderer year project-name
+  (:require [leiningen.new.templates :refer [renderer sanitize year project-name
                                              ->files sanitize-ns name-to-path
                                              multi-segment]]
             [leiningen.core.main :as main]))
@@ -10,6 +10,7 @@
   (let [render  (renderer "amp")
         main-ns (multi-segment (sanitize-ns name))
         data    { :raw-name    name
+                  :sanitized-name (sanitize name)
                   :name        (project-name name)
                   :namespace   main-ns
                   :nested-dirs (name-to-path main-ns)
@@ -17,23 +18,30 @@
     (main/info "Generating a project called" name "based on the 'amp' template.")
     (main/info "To see other templates (app, plugin, etc), try `lein help new`.")
     (->files data
+             ; Standard project files
              [".gitignore"  (render "gitignore" data)]
              ["project.clj" (render "project.clj" data)]
              ["README.md"   (render "README.md" data)]
              ["LICENSE"     (render "LICENSE" data)]
+
+             ; Standard Clojure project structure
              "src"
-             "src/amp"
-             ["src/amp/module.properties" (render "module.properties" data)]
-             "src/amp/web"
-             "src/amp/web/css/{{namespace}}"
-             "src/amp/web/images/{{namespace}}"
-             "src/amp/web/scripts/{{namespace}}"
-             "src/amp/config"
-             "src/amp/config/alfresco/extension/templates/webscripts/{{namespace}}"
-             "src/amp/module"
-             ["src/amp/module/module-context.xml" (render "module-context.xml" data)]
-             "src/amp/module/context"
-             "src/amp/licenses"
              "src/clojure"
              ["src/clojure/{{nested-dirs}}.clj" (render "core.clj" data)]
-             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)])))
+             ["test/{{nested-dirs}}_test.clj" (render "test.clj" data)]
+             "src/resource"
+
+             ; AMP specific project structure
+             "src/resource/alfresco/extension/templates/webscripts/{{sanitized-name}}"
+             "src/resource/alfresco/module/{{sanitized-name}}"
+             ["src/resource/alfresco/module/{{sanitized-name}}/module-context.xml" (render "module-context.xml" data)]
+             "src/resource/alfresco/module/{{sanitized-name}}/context"
+             "src/resource/licenses"
+
+             "src/amp"
+             ["src/amp/module.properties" (render "module.properties" data)]
+             "src/resource/META-INF/resources"
+             "src/resource/META-INF/resources/html/{{sanitized-name}}"
+             "src/resource/META-INF/resources/css/{{sanitized-name}}"
+             "src/resource/META-INF/resources/images/{{sanitized-name}}"
+             "src/resource/META-INF/resources/scripts/{{sanitized-name}}")))
--- src/leiningen/new/amp/project.clj
+++ src/leiningen/new/amp/project.clj
@@ -1,14 +1,15 @@
-(defproject {{raw-name}} "0.1.0-SNAPSHOT"
+(defproject {{sanitized-name}} "0.1.0-SNAPSHOT"
   :title "FIXME: write title"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license { :name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html" }
   :min-lein-version "2.0.0"
-  :profiles { :dev { :plugins [[lein-amp "0.3.0"]] }
+  :profiles { :dev { :plugins [[lein-amp "0.4.0-SNAPSHOT"]] }
               :provided { :dependencies [
                                           [org.clojure/clojure "1.6.0" :scope "runtime"]
 ;                                          [org.clojars.lambdalf/lambdalf "2.0.0-SNAPSHOT" :scope "runtime"]   ; Not yet available on clojars...
                                         ] }}
-  :source-paths ["src/clojure"]
+  :source-paths    ["src/clojure"]
+  :resource-paths  ["src/resource"]
   :amp-source-path "src/amp")
