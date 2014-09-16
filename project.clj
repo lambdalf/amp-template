38133cade459d14e88dc1cff2536105404bbd7f0
Peter Monks <pmonks@gmail.com>
2014-09-15 17:40:59 -0700

    Rolled version ahead of v0.8.0 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amp/lein-template "0.7.0"
+(defproject amp/lein-template "0.8.0-SNAPSHOT"
   :description "Leiningen template for AMP (Alfresco Module Package) projects."
   :url "https://github.com/lambdalf/amp-template"
   :license {:name "Eclipse Public License"
